5760acbb0157be8fbf4204aef1d46fbb9a3b32a6
DanieleChagasSouza <DanieleChagasSouza@DESKTOP-JA9PRHV>
2024-05-03 11:10:26 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoDesafio1/Modelos/Pessoa.cs
@@ -0,0 +1,48 @@
+﻿namespace ProjetoDesafio1.Modelos;
+
+public class Pessoa
+{
+    public string Nome { get; set; }
+
+    public int  DataDeNascimento { get; set; }
+
+    public double Altura { get; set; }
+
+    public Pessoa()
+    {
+    }
+
+    public int CalcularIdadeDaPessoa()
+    {
+        int diaNascimento = DataDeNascimento % 100;
+        int anoNascimento = DataDeNascimento / 10000;
+        int mesNascimento = (DataDeNascimento / 100) % 100;
+
+        try
+        {
+            DateTime dataNascimento = new DateTime(anoNascimento, mesNascimento, diaNascimento);
+            DateTime hoje = DateTime.Today;
+
+            int idade = hoje.Year - dataNascimento.Year;
+            if (dataNascimento > hoje.AddYears(-idade))
+                idade--;
+            return idade;
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Data de nascimento inválida.");
+            return -1;
+        }
+
+
+    }
+
+    public void ExibirDadosDeUmaPessoa()
+    {
+        Console.WriteLine($"Meu Nome é: {Nome}");
+        Console.WriteLine($"Minha data de nascimento é: {DataDeNascimento.ToString("0000/00/00")}");
+        Console.WriteLine($"Minha idade é: {CalcularIdadeDaPessoa()}");
+        Console.WriteLine($"Minha altura é {Altura.ToString("0.00")} M");
+    }
+
+}
---
+++ ProjetoDesafio1/Program.cs
@@ -0,0 +1,7 @@
+﻿using ProjetoDesafio1.Modelos;
+
+Pessoa pessoa = new Pessoa();
+pessoa.Nome = "Daniele";
+pessoa.Altura = 1.64;
+pessoa.DataDeNascimento = 19911125;
+pessoa.ExibirDadosDeUmaPessoa();
